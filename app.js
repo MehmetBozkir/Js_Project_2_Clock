de109ca06c50e1f0ee2f652947356ed77c2f9be4
MehmetBozkir <mehmetsalihbozkir@gmail.com>
2023-12-14 18:53:38 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,197 @@
+function setDate() {
+  const now = new Date();
+
+  const seconds = now.getSeconds();
+  const secondsDegrees = (seconds / 60) * 360 + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const mins = now.getMinutes();
+  const minsDegrees = (mins / 60) * 360 + (seconds / 60) * 6 + 90;
+  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+
+  const hour = now.getHours();
+  const hourDegrees = (hour / 12) * 360 + (mins / 60) * 30 + 90;
+  hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+
+  const milliseconds = now.getMilliseconds();
+  const milliDegrees = (milliseconds / 1000) * 360 + 90;
+  milliHand.style.transform = `rotate(${milliDegrees}deg)`;
+
+  //console.log(second);
+}
+
+setInterval(setDate, 1);
+
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+
+varx = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+
+varx = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+
+varx = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+
+varx = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+
+varx = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+
+varx = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
+x = 1;
